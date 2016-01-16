fd5f8132214933ef2fb69cfe21aaca9a393ec368
PavelNPavlov <pavelnapavlov@gmail.com>
2016-01-16 09:30:23 +0200

    Added functionality for switching between game stages

--- src/Magic-Game/app/src/main/java/com/example/lexusus/magic_game/GameActivity.java
+++ src/Magic-Game/app/src/main/java/com/example/lexusus/magic_game/GameActivity.java
@@ -2,18 +2,71 @@ package com.example.lexusus.magic_game;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
 
-    TilePuzzleFragment tilePuzzle;
+    private int mStage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        this.mStage = 1;
+        InitGame();
     }
 
 
 
-    private void addMainFragment() {
+
+    private void InitGame() {
+        getFragmentManager()
+                .beginTransaction()
+                .add(R.id.fl_game_area, new StageOne())
+                .commit();
+    }
+
+    private void ChangeStage(){
+        this.mStage += 1;
+        if(mStage>3){
+            this.mStage = 1;
+        }
+
+        switch (this.mStage){
+            case 1:
+                SwitchGameFragment(1);
+                break;
+            case 2:
+                SwitchGameFragment(2);
+                break;
+            case 3:
+                SwitchGameFragment(3);
+                break;
+        }
+    }
+
+    private void SwitchGameFragment(int i) {
+        if(i==1){
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fl_game_area, new StageOne())
+                    .commit();
+        }
+        if(i==2){
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fl_game_area, new StageTwo())
+                    .commit();
+        }
+        if(i==3){
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fl_game_area, new StageThree())
+                    .commit();
+        }
+    }
+
+    public void Test(View view) {
+        ChangeStage();
     }
 }
