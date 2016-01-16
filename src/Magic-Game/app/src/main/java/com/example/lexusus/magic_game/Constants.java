04ce5c2eb5f4380a2deb2c1b907852b1383e3514
PavelNPavlov <pavelnapavlov@gmail.com>
2016-01-16 11:29:18 +0200

    Stage two functionality added. Speech Listen on button press. Set of spells added. On correct spell switch to next stage

---
+++ src/Magic-Game/app/src/main/java/com/example/lexusus/magic_game/Constants.java
@@ -0,0 +1,11 @@
+package com.example.lexusus.magic_game;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Constants {
+    static final Set<String> SPELLS = new HashSet<String>(Arrays.asList(
+            new String[]{"fireball", "iceshard"}
+    ));
+}
--- src/Magic-Game/app/src/main/java/com/example/lexusus/magic_game/GameActivity.java
+++ src/Magic-Game/app/src/main/java/com/example/lexusus/magic_game/GameActivity.java
@@ -1,11 +1,18 @@
 package com.example.lexusus.magic_game;
 
 import android.app.Fragment;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -19,6 +26,8 @@ public class GameActivity extends AppCompatActivity {
     private Tiles[] tiles;
     private int[] power;
     private int pairsLeft;
+
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +47,47 @@ public class GameActivity extends AppCompatActivity {
 
     }
 
-    //Flip Animations
+    //Speech
+
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    String spell = result.get(0);
+                    Log.d("test",result.get(0));
+                    if(Constants.SPELLS.contains(spell)){
+                        this.ChangeStage();
+                    }
+                }
+                break;
+            }
+
+        }
+    }
+
+    //Tile Match Game
     public void Flip(View view){
 
         String tag = (String)view.getTag();
@@ -214,5 +263,9 @@ public class GameActivity extends AppCompatActivity {
 
     public void Test(View view) {
         ChangeStage();
+    }
+
+    public void Listen(View view) {
+        this.promptSpeechInput();
     }
 }
