7731672a77ad2a096a87b4054f245bf0e3c80691
alexizvely <alexizvely@gmail.com>
2016-01-17 04:36:26 +0200

    profile page with animated avatar

--- src/Magic-Game/app/src/main/java/com/example/lexusus/magic_game/MainMenuActivity.java
+++ src/Magic-Game/app/src/main/java/com/example/lexusus/magic_game/MainMenuActivity.java
@@ -5,13 +5,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    private StaticAnimateGameView menuView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        menuView = new StaticAnimateGameView(this,R.drawable.cute,82,118,8);
 
         getFragmentManager()
                 .beginTransaction()
@@ -35,5 +39,28 @@ public class MainMenuActivity extends AppCompatActivity {
     }
     public void onClickGameRules(View view){
         Log.d("my app", "game rules");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Tell the gameView resume method to execute
+        menuView.resume();
+    }
+
+    // This method executes when the player quits the game
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Tell the gameView pause method to execute
+        menuView.pause();
+    }
+
+    public void AddAnimatedAvatar(RelativeLayout rl) {
+        // Initialize gameView and set it as the view
+
+        rl.addView(menuView);
     }
 }
--- src/Magic-Game/app/src/main/java/com/example/lexusus/magic_game/ProfileFragment.java
+++ src/Magic-Game/app/src/main/java/com/example/lexusus/magic_game/ProfileFragment.java
@@ -7,19 +7,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 /**
  * Created by Lexusus on 1/13/2016.
  */
 public class ProfileFragment extends Fragment {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profile_fragment,container, false);
+        View view = inflater.inflate(R.layout.profile_fragment,container, false);
+        RelativeLayout rl = (RelativeLayout)view.findViewById(R.id.rl_avatar);
+        MainMenuActivity activity = (MainMenuActivity)getActivity();
+        activity.AddAnimatedAvatar(rl);
+        return view;
     }
 }
